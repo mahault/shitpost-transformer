b20aa0777f8ef948053030bf589187cd85238474
mahault <42900146+mahault@users.noreply.github.com>
2022-01-09 12:56:32 -0500

    step 5

--- transformer.py
+++ transformer.py
@@ -40,4 +40,30 @@ class Decoder(nn.Module):
             output = layer(output, enc, tgt_mask, enc_mask)
         return output
     
-    
+class EncoderLayer(nn.module):
+    def __init__(self, d_model, num_heads, d_ff=2048, dropout=0.3):
+        super().__init__()
+        #attention
+        self.attn = MultiHeadedAttention(d_model, num_heads, dropout=dropout)
+        #ffn
+        self.ffn = nn.Sequential(
+            nn.Linear(d_model, d_ff),
+            nn.ReLU(inplace=True),
+            nn.Dropout(dropout),
+            nn.Linear(d_ff, d_model),
+            nn.Dropout(dropout),
+        )
+        
+        # layer norm
+        self.attn_norm = nn.LayerNorm(d_model)
+        self.ffn_norm = nn.LayerNorm(d_model)
+        
+    def forward(self, src, src_mask):
+        x = src
+        x = x+ self.attn(q=x, k=x, v=x, mask=src_mask)
+        x = self.attn_norm(x)
+        x = self.ffn(x)
+        x = self.ffn_norm(x)
+        return x
+
+
