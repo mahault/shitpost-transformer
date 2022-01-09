44fccc52cacd08049409cbd94e2a2eb3c6bde98a
mahault <42900146+mahault@users.noreply.github.com>
2022-01-09 15:47:05 -0500

    tranfor

--- transformer.py
+++ transformer.py
@@ -66,6 +66,55 @@ class EncoderLayer(nn.module):
         x = self.ffn_norm(x)
         return x
 
+
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff=2048, dropout=0.3):
+        super().__init__()
+        
+        #masked attn
+        self.masked_attn = MultiHeadedAttention(
+            d_model, num_heads, dropout=dropout
+        )
+        
+        #attn
+        self.attn = MultiHeadedAttention(
+            d_model, num_heads, dropout=dropout
+        )
+        
+        #ffn
+        self.ffn = nn.Sequential(
+            nn.Linear(d_model, d_ff),
+            nn.ReLU(inplace=True),
+            nn.Dropout(dropout),
+            nn.Linear(d_ff, d_model),
+            nn.Dropout(dropout),
+        )
+        
+        #layer norm
+        self.masked_attn_norm = nn.LayerNorm(d_model)
+        self.attn_norm = nn.LayerNorm(d_model)
+        self.ffn_norm = mm.LayerNorm(d_model)
+        
+    def forward(self, tgt, enc, tgt_mask, enc_mask):
+        x = tgt
+        x = x + self.masked_attn(q=x, k=x, v=x, mask=tgt_mask)
+        x = self.masked_attn_norm(x)
+        x = x + self.attn(q=x, k=enc, v=enc, mask=enc_mask)
+        x = self.attn_norm(x)
+        x = x + self.ffn(x)
+        x = self.ffn_norm(x)
+        return 
+    
+    if mask is not None:
+        expanded_mask = mask[:, None, :].expand(bs, tgt_len, seq_len)
+        susequent_mask = 1 - torch.triu(
+            torch.ones((tgt_len, tgt_len), device=mask.device, dtype=torch.uint8), diagonal=1
+        )
+        subsequent_mask = subsequent_mask[None, :, :].expand(bs, tgt_len, tgt_len)
+        scores = scores.masked_fill(expanded_mask == 0, -float("Inf"))
+        scores = scores.masked_fill(subsequent_mask == 0, -float("Inf"))
+
+
 class MultiHeadedAttention(nn.Module):
     def __init__(self, d_model, num_heads, dropout):
         super().__init__()
@@ -109,4 +158,32 @@ class SelfAttention(nn.Module):
         dim_k = key.size(-1)
         scores = torch.bmm(query, key.transpose(1,2)) / nq.sqrt(dim_k)
         
+        if mask is not None:
+            expanded_mask = mask[:, None, :].expand(bs, tgt_len, seq_len)
+            scores = scores.masked_fill(expand_mask == 0, -float("Inf"))
         
+        weights = F.softmax(scores, dim=-1)
+        outputs = torch.bmm(weights, value)
+        return outputs
+    
+    
+    
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=50000):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.) / d_model))  # Need to look into this
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return self.dropout(x + self.pe[:x.size(0)])
+    
+    
+
+trans = Transformer()
+trans.forward()
