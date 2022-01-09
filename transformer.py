8bb1c7791606928e9334d8cc7b8fe74364f3c977
mahault <42900146+mahault@users.noreply.github.com>
2022-01-09 13:17:55 -0500

    step 6

--- transformer.py
+++ transformer.py
@@ -66,4 +66,27 @@ class EncoderLayer(nn.module):
         x = self.ffn_norm(x)
         return x
 
-
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, d_model, num_heads, dropout):
+        super().__init__()
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.dropout = dropout
+        self.attn_output_size = self.d_model // self.num_heads
+        self.attentions = nn.ModuleList(
+            [
+                SelfAttention(d_model, self.attn_output_size)
+                for _ in range(self.num_heads)
+            ],
+        )
+        self.output = nn.Linear(self.d_model, self.d_model)
+    
+    def forward(self, q, k, v, mask):
+        x = torch.cat(
+            [
+                layer(q,k,v,mask) for layer in self.attentions
+            ],dim=-1
+        )
+        x = self.output(x)
+        return x
+        
